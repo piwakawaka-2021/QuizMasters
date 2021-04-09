fb08c0243f1ea44ecbcac215e66a4ae789e3590e
alex-bartleynees <73970031+alex-bartleynees@users.noreply.github.com>
2021-04-09 15:00:19 +1200

    Remove h2 heading" "

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,7 +8,6 @@ function App () {
       <div className='title'>
 
         <h1>Ridiculous questions and ridiculous answers</h1>
-        <h2>Quiz Board</h2>
       </div>
       {/* This 'main' div is only for styling (so we can use flexbox) */}
       <div className='main'>
